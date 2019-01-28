c177e9c77230bf8e904db58f25b12ba6e0362fc9
Samuel Adesina <39684294+asoluwaseun@users.noreply.github.com>
2019-01-28 23:52:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ var form = new fm.IncomingForm();
 var bodyParser = require('body-parser');
 chat.use(bodyParser.json());
 chat.use(bodyParser.urlencoded({extended:true}));
-
+chat.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 //mongoose requirement
 var mongoose = require('mongoose');
 
