9dcfa048f02f6a1a2a490a4d10a90b775b06dbea
Samuel Adesina <39684294+asoluwaseun@users.noreply.github.com>
2019-01-29 12:53:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var bodyParser = require('body-parser');
 chat.use(bodyParser.json());
 chat.use(bodyParser.urlencoded({extended:true}));
 chat.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost");
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
